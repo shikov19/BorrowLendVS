6c680fc3f910920547984b91afaa561e375c2bc1
Samuil Dobrinski <msamuil@abv.bg>
2021-09-21 14:01:12 +0300

    Add project files.

---
+++ BorrowLend/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BorrowLend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BorrowLend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Item> items { get; set; }
+    }
+}
---
+++ BorrowLend/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BorrowLend.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name ="Item name")]
+        public string ItemName { get; set; }
+        [Required]
+        public string Borrower { get; set; }
+        [Required]
+        public string Lender { get; set; }
+
+
+    }
+}
